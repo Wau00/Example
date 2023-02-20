84b6ca4537871ddffc822a46a27847b4cc1d6c99
Walter Underwood <99919050+Wau00@users.noreply.github.com>
2023-02-19 21:54:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ var cuantoTeAmo = 1;
 
 
 
-var num = document.getElementById("howmuch")
-function howMuch() {
-    setTimeout(howMuch, 1000);
-    setInterval(increase, 1000);
-}
-function increase() {
-    if (cuantoTeAmo < 1000000) {
-        cuantoTeAmo++;
-        num.innerHTML = cuantoTeAmo;
-        console.log("Te amo esto:" + cuantoTeAmo);
-    }
+// var num = document.getElementById("howmuch")
+// function howMuch() {
+//     setTimeout(howMuch, 1000);
+//     setInterval(increase, 1000);
+// }
+// function increase() {
+//     if (cuantoTeAmo < 1000000) {
+//         cuantoTeAmo++;
+//         num.innerHTML = cuantoTeAmo;
+//         console.log("Te amo esto:" + cuantoTeAmo);
+//     }
+// }
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+    const d = new Date();
+    document.getElementById("howmuch").innerHTML = d.valueOf();
 }
